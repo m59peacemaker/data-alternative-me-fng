c3f9cc822e577fecf252fd6d925af7c8fcc26c85
Johnny Hauser <johnnyhauser@gmail.com>
2018-11-07 14:36:55 -0600

    no need to set upstream

--- lib/commit-data.sh
+++ lib/commit-data.sh
@@ -10,5 +10,5 @@ if [ -n "`git status fng.yaml -s`" ]; then
 	git add fng.yaml
 	git commit -m "FnG data update `date --iso-8601=minutes` [ci skip]"
 	git remote add data https://${GITHUB_TOKEN}@github.com/m59peacemaker/data-alternative-me-fng.git > /dev/null 2>&1
-	git push --quiet -u data master
+	git push --quiet data master
 fi
