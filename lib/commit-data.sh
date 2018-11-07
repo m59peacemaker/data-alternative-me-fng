2acab8cf70b2d871537719f6b327730162892150
Travis CI <travis@travis-ci.org>
2018-11-07 15:35:08 -0600

    automated update is not pushing to repo but acts like it is...maybe need to checkout master?

--- lib/commit-data.sh
+++ lib/commit-data.sh
@@ -7,6 +7,7 @@ git config user.name "Travis CI"
 
 if [ -n "`git status fng.yaml -s`" ]; then
 	echo 'comitting new data to git...'
+	git checkout master
 	git add fng.yaml
 	git commit -m "FnG data update `date --iso-8601=minutes` [ci skip]"
 	git remote add data https://${GITHUB_TOKEN}@github.com/m59peacemaker/data-alternative-me-fng.git > /dev/null 2>&1
