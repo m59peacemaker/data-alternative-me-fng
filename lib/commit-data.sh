ba0bfe4b6bcf141cf317002d606f00cdb73782a7
Johnny Hauser <johnnyhauser@gmail.com>
2018-11-07 14:34:41 -0600

    correct env var name

--- lib/commit-data.sh
+++ lib/commit-data.sh
@@ -9,6 +9,6 @@ if [ -n "`git status fng.yaml -s`" ]; then
 	echo 'comitting new data to git...'
 	git add fng.yaml
 	git commit -m "FnG data update `date --iso-8601=minutes` [ci skip]"
-	git remote add data https://${GH_TOKEN}@github.com/m59peacemaker/data-alternative-me-fng.git > /dev/null 2>&1
+	git remote add data https://${GITHUB_TOKEN}@github.com/m59peacemaker/data-alternative-me-fng.git > /dev/null 2>&1
 	git push --quiet -u data master
 fi
