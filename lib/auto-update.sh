03589351b45a486ad742425ad274fb7db8b551b6
Johnny Hauser <johnnyhauser@gmail.com>
2018-11-07 13:57:53 -0600

    travis ci cron job setup

---
+++ lib/auto-update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then;
+	exit
+fi
+
+./lib/update-local-data.mjs && ./lib/commit-data.sh
--- lib/commit-data.sh
+++ lib/commit-data.sh
@@ -9,6 +9,6 @@ if [[ -z $(git status fng.yaml -s) ]]; then
 	echo 'comitting new data to git...'
 	git add fng.yaml
 	git commit -m "FnG data update `date --iso-8601=minutes`"
-	git remote add data https://${GH_TOKEN}@github.com/m59peacemaker/alternative-me-fng-data.git > /dev/null 2>&1
+	git remote add data https://${GH_TOKEN}@github.com/m59peacemaker/data-alternative-me-fng.git > /dev/null 2>&1
 	git push --quiet -u data master
 fi
