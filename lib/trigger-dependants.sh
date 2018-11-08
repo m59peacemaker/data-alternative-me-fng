f3a106cf09d8a9c88f3915fb8d664520d7c5ac98
Johnny Hauser <johnnyhauser@gmail.com>
2018-11-08 10:12:32 -0600

    added travis api token

--- lib/trigger-dependants.sh
+++ lib/trigger-dependants.sh
@@ -5,14 +5,12 @@ set -e
 repos=" \
 	pine-alternative-me-fng \
 "
-token=`travis token --com`
-
 for repo in $repos; do
 	curl -s -X POST \
 		-H "Content-Type: application/json" \
 		-H "Accept: application/json" \
 		-H "Travis-API-Version: 3" \
-		-H "Authorization: token $token" \
+		-H "Authorization: token $TRAVIS_CI_API_TOKEN" \
 		-d '{ "request": { "branch": "master" } }' \
 		https://api.travis-ci.com/repo/m59peacemaker%2F$repo/requests
 done
