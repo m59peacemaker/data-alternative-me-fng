c3f296d496475d074f733becc5b40b388220b209
Johnny Hauser <johnnyhauser@gmail.com>
2018-11-15 21:50:30 -0600

    more logging about triggering the dependant ci build

--- lib/trigger-dependants.sh
+++ lib/trigger-dependants.sh
@@ -5,12 +5,17 @@ set -e
 repos=" \
 	pine-alternative-me-fng \
 "
+
 for repo in $repos; do
+	repo_url="https://api.travis-ci.org/repo/m59peacemaker%2F${repo}/requests"
+
+	echo "triggering ci build for ${repo}..."
+
 	curl -s -X POST \
 		-H "Content-Type: application/json" \
 		-H "Accept: application/json" \
 		-H "Travis-API-Version: 3" \
-		-H "Authorization: token $TRAVIS_CI_API_TOKEN" \
+		-H "Authorization: token ${TRAVIS_CI_API_TOKEN}" \
 		-d '{ "request": { "branch": "master" } }' \
-		https://api.travis-ci.org/repo/m59peacemaker%2F$repo/requests
+		${repo_url}
 done
